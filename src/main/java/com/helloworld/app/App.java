360e35799bfa5ba4dc1677711e832b412881e115
fandhz <66157373+fandhz@users.noreply.github.com>
2025-03-09 19:27:42 +0700

    Update App.java  | adding vuln code

--- src/main/java/com/helloworld/app/App.java
+++ src/main/java/com/helloworld/app/App.java
@@ -1,5 +1,5 @@
 package com.helloworld.app;
-
+import java.io.*;
 /**
  * Hello world!
  *
@@ -9,5 +9,36 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+}
+
+
+
+public class VulnerableApp {
+    public static void main(String[] args) {
+
+        String password = "12345678"; 
+
+        
+        if (args.length > 0 && args[0].equals(password)) {
+            System.out.println("Access granted.");
+        } else {
+            System.out.println("Access denied.");
+        }
+
+        String userInput = args.length > 1 ? args[1] : "";
+        String command = "ping " + userInput; 
+
+        try {
+            Process process = Runtime.getRuntime().exec(command);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            process.waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
